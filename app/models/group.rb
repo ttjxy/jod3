aca95e26ff7ac3db2de6138c3fdab62c19baebad
xy <ttjxy@gmail.com>
2017-05-01 02:29:18 +0800

    validate group descriptions & post content, title && admin group show change @post to post

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
   validates :title, presence: true
+  validates :description, presence:true
+  validates :description2, presence:true
   belongs_to :user
   has_many :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
   validates :content, presence: true
+  validates :content2, presence: true
+  validates :content3, presence: true
+  validates :content4, presence: true
 end
