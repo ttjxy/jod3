588206d81e6b7e8f8c6778f8bd3c9c3bb7da1c0d
xy <ttjxy@gmail.com>
2017-04-30 16:07:44 +0800

    have a try 2

--- db/migrate/20170427161042_add_more_detail_to_post.rb
+++ db/migrate/20170427161042_add_more_detail_to_post.rb
@@ -1,13 +1,6 @@
 class AddMoreDetailToPost < ActiveRecord::Migration[5.0]
   def change
     add_column :posts, :title, :string
-    add_column :posts, :Objective, :text
-    add_column :posts, :Reflective, :text
-    add_column :posts, :Interpretive, :text
-    add_column :posts, :Decisional, :text
-    delete_column :posts, :Objective, :text
-    delete_column :posts, :Reflective, :text
-    delete_column :posts, :Interpretive, :text
-    delete_column :posts, :Decisional, :text
+    
   end
 end
